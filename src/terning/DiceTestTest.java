af3a553ec75d2289f4a5cf40435a7c39b08d3908
kalkaran <neal.norman@gmail.com>
2018-10-05 11:55:30 +0200

    rollmultiple fix

--- src/terning/DiceTestTest.java
+++ src/terning/DiceTestTest.java
@@ -1,5 +1,62 @@
+package terning;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DiceTestTest {
 
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void roll() {
+        Dice test = new Dice();
+        int count = 60000;
+        int one, two, three, four, five, six;
+        one = two = three = four =  five = six = 0;
+        System.out.println("one: " + one);
+        System.out.println("two: " + two);
+        System.out.println("three: " + three);
+        System.out.println("four: " + four);
+        System.out.println("five: " + five);
+        System.out.println("six: " + six);
+        int result;
+        while(count > 0){
+            result = test.roll();
+            switch (result) {
+                case 1:
+                    one++;
+                    break;
+                case 2:
+                    two++;
+                    break;
+                case 3:
+                    three++;
+                    break;
+                case 4:
+                    four++;
+                    break;
+                case 5:
+                    five++;
+                    break;
+                case 6:
+                    six++;
+                    break;
+            }
+            count--;
+        }
+        System.out.println("one: " + one);
+        System.out.println("two: " + two);
+        System.out.println("three: " + three);
+        System.out.println("four: " + four);
+        System.out.println("five: " + five);
+        System.out.println("six: " + six);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void rollMultiple() {
+
+    }
 }
